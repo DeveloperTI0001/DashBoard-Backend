52a1adf416a0317efddc515246aee2706f5ec731
Carlos <69126001+DeveloperTI0001@users.noreply.github.com>
2025-09-18 00:31:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-// Archivo: index.js
-const express = require('express');
-const app = express();
+// En tu index.js
 const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
-
-app.get('/api', (req, res) => {
-  res.json({ mensaje: "¡Hola desde Render!" });
-});
-
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
