f3103d4eca638ed19b8afaace75a9a6d8c35606a
Carlos <martin5435412@gmail.com>
2025-09-18 14:41:32 -0500

    .

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-// Archivo: index.js
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5176;
+const session = require('express-session');
+const passport = require('passport');
+const authRoutes = require('./auth');
+const DiscordStrategy = require('./src/strategies/discordStrategy');
 
-app.use(express.json());
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  cookie: { maxAge: 60000 * 60 * 24 },
+  saveUninitialized: false,
+  resave: false, // Añadido para evitar advertencias de sesión
+}));
 
-app.get('/api', (req, res) => {
-  res.json({ mensaje: "¡Hola desde Render!" });
-});
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/auth', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
