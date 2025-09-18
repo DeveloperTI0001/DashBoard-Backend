a95abb0195d9bceb4f1da83cb8fa723492398415
Carlos <martin5435412@gmail.com>
2025-09-18 15:11:27 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -7,7 +7,6 @@ router.get('/', passport.authenticate('discord'));
 router.get('/redirect', passport.authenticate('discord', { 
     failureRedirect: '/forbidden' 
 }), (req, res) => {
-  res.status(200).send('OK'); // Envía el código 200 y el texto 'OK'
   res.redirect('/settings');
 });
 
