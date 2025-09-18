357d8da171cbcb847072dc0ee32ed5040daf324a
Carlos <martin5435412@gmail.com>
2025-09-18 15:28:40 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const passport = require('passport');
+router.get('/', passport.authenticate('discord'));
 
 router.get('/redirect', passport.authenticate('discord', { 
     failureRedirect: '/forbidden' 
